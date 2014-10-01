0e9d3e310b85960bfe97f3903e66a16e317a1e41
iamijaz <ijaz.ahmed@msn.com>
2014-10-01 12:08:56 +0100

    added build.cmd & build.ps1

---
+++ GG.Web.HelloMVCWorld/build.cmd
@@ -0,0 +1,11 @@
+﻿rem Copy this file to start making use of the shared build script. There should be no need to edit it.
+rem Confluence documentation: https://justgiving.atlassian.net/wiki/display/DD/A%3A+Building+and+Testing+with+Shared+Build+Script+and+TeamCity
+@echo off
+
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -command ".\build.ps1 -task %1 ;exit $LASTEXITCODE"
+
+if %ERRORLEVEL% == 0 goto OK
+	echo Error running build. 
+exit /B %ERRORLEVEL%
+
+:OK
---
+++ GG.Web.HelloMVCWorld/build.ps1
@@ -0,0 +1,22 @@
+﻿param([string]$task = "local")
+# Copy this file to start making use of the shared build script. Edit the ApplicationName, ChocolateyPackages and NugetPackages.
+# Confluence documentation: https://justgiving.atlassian.net/wiki/display/DD/A%3A+Building+and+Testing+with+Shared+Build+Script+and+TeamCity
+
+$ApplicationName = "GG.Example.Microservice"
+
+#$ChocolateyPackages = @(
+#	"GG.Example.Microservice.Web"
+#)
+
+#$NugetPackages = @(
+#	"GG.Example.Microservice.Client"
+#)
+
+$webClient = new-object net.webclient
+$webClient.Headers.Add("Accept", "application/vnd.github.3.raw")
+$webClient.Headers.Add("User-Agent", "JustGivingBuildScript")
+$webClient.DownloadFile('https://api.github.com/repos/justgiving/GG.BuildScript/contents/bootstrap.ps1?access_token=b8d04243394ffcb43d4295fb066ca77a3bd1d443','bootstrap.ps1')
+
+. .\bootstrap.ps1
+
+Invoke-Build -Task $task -ApplicationName $ApplicationName #-ChocolateyPackages $ChocolateyPackages -NugetPackages $NugetPackages
